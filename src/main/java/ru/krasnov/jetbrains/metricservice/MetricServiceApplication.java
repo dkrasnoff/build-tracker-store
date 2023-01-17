893edc3b880b642f5f2712f649e047603d6e2688
Dmitrii Krasnov <dv.krasn0v@yandex.ru>
2023-01-17 18:05:31 +0400

    init commit

---
+++ src/main/java/ru/krasnov/jetbrains/metricservice/MetricServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.krasnov.jetbrains.metricservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/krasnov/jetbrains/metricservice/MetricServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.krasnov.jetbrains.metricservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
